cd5a31db7956938033d0da4a96303b98b87615bd
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-05 22:56:27 +0600

    Add general message

--- index.js
+++ index.js
@@ -3,23 +3,32 @@
 var PSON = require('pson');
 var pson = new PSON.StaticPair(require('./dictionary.json'));
 
+function Message(dest, data) {
+    return pson.encode({ dest: dest, data: data });
+}
+
+Message.Welcome = function Welcome(id) {
+    return pson.encode({ type: 'WELCOME', id: id });
+};
+
+Message.Trace = function Trace(position, who) {
+    return pson.encode({ type: 'TRACE', position: position, id: who });
+};
+
+Message.Connected = function Connected(who, position) {
+    return pson.encode({ type: 'CONNECTED', id: who, position: position });
+};
+
+Message.Disconnected = function Disconnected(who) {
+    return pson.encode({ type: 'DISCONNECTED', id: who });
+};
+
+Message.Log = function Log(data) {
+    return pson.encode({ type: 'LOG', data: data });
+};
+
+Message.decode = pson.decode.bind(pson);
+
 module.exports = {
-    Messages: {
-        Welcome: function Welcome(id) {
-            return pson.encode({ type: 'WELCOME', id: id });
-        },
-        Trace: function Trace(position, who) {
-            return pson.encode({ type: 'TRACE', position: position, id: who });
-        },
-        Connected: function Connected(who, position) {
-            return pson.encode({ type: 'CONNECTED', id: who, position: position });
-        },
-        Disconnected: function Disconnected(who) {
-            return pson.encode({ type: 'DISCONNECTED', id: who });
-        },
-        Log: function Log(payload) {
-            return pson.encode({ type: 'LOG', payload: payload });
-        },
-        decode: pson.decode.bind(pson)
-    }
+    Messages: Message
 };
--- test/index.js
+++ test/index.js
@@ -8,6 +8,17 @@ it('should require protocols without errors', function () {
     require('../');
 });
 
+it('should encode and decode general message', function () {
+    var Message = require('../').Messages;
+
+    var encoded = new Message({}, { some: 'data' });
+    should.exist(encoded);
+
+    var decoded = Message.decode(encoded.compact());
+    decoded.dest.should.eql({});
+    decoded.data.some.should.eql('data');
+});
+
 it('should encode and decode Welcome message', function () {
     var Message = require('../').Messages;
 
